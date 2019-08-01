20c8d4746ddfc9c32fd3fcc2304e5745241abf91
chenjr <chenjr@live.cn>
2019-08-01 16:46:11 +0800

    fix typo

--- golist.go
+++ golist.go
@@ -5,7 +5,7 @@ import "fmt"
 import "github.com/chenjr15/golist/linkedlist"
 
 func main() {
-	a := linkedlist.MakeLinkList([]int{1, 2, 3})
+	a := linkedlist.MakeLinkedList([]int{1, 2, 3})
 
 	fmt.Println("golist")
 	fmt.Println(a)
